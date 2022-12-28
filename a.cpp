e9562832f862fe5e80e1c99a86abbfea46e0f7f5
Venom-61 <subhendukarmakar61@gmail.com>
2022-12-28 23:58:46 +0530

    Marathon

---
+++ a.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+// A. Marathon
+
+void solve() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << (a < b) + (a < c) + (a < d) << "\n";
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int test = 1;
+    cin >> test;
+    for(int i=1; i<=test; i++) {
+        solve();
+    }
+
+    return 0;
+}
