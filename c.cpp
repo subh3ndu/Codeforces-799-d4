d5756ad4c5d1bb360fea73d0381b16b62eea98a4
Venom-61 <subhendukarmakar61@gmail.com>
2022-12-29 00:01:47 +0530

    Where's the bishop

---
+++ c.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+// C. Where's the Bishop ?
+
+void solve() {
+    
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int test = 1;
+    cin >> test;
+    for(int i=1; i<=test; i++) {
+        solve();
+    }
+
+    return 0;
+}
