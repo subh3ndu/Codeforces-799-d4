69e1d247ab91093be0f6eb74549a4057460a38dc
Venom-61 <subhendukarmakar61@gmail.com>
2022-12-31 03:50:10 +0530

    Where's the bishop?OC

--- c.cpp
+++ c.cpp
@@ -6,7 +6,21 @@ typedef long long ll;
 // C. Where's the Bishop ?
 
 void solve() {
-    
+    char a[8][8];
+    for(int i = 0; i < 8; i++){
+        for(int j = 0; j < 8; j++){
+            cin >> a[i][j];
+        }
+    }
+
+    for(int i = 0; i < 8; i++){
+        for(int j = 0; j < 8; j++){
+            if(a[i][j] == '#' && a[i+1][j] == '.' && a[i+2][j] == '#'){
+                cout << i+2 << " " << j+2 << "\n";
+                break;
+            }
+        }
+    }
 }
 
 int32_t main() {
